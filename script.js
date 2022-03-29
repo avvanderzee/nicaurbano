df504dba53238bece8f752f93305b609ffcade4a
Scott Martinez <scottmartinezolivas@hotmail.com>
2022-03-29 15:55:52 -0600

    cambios enla vista movil

--- script.js
+++ script.js
@@ -1,37 +1,36 @@
 const hamburger = document.querySelector('.ham');
-const enlaces= document.querySelector('.enlaces-menu') ;
-const barras=document.querySelectorAll('.ham span');    //select all the span elements in the hamburger
+const enlaces = document.querySelector('.enlaces-menu');
+const barras = document.querySelectorAll('.ham span'); //select all the span elements in the hamburger
 hamburger.addEventListener('click', () => {
 
     enlaces.classList.toggle('activado');
     barras.forEach(barra => {
         barra.classList.toggle('animado');
     });
-})   
+})
 
 //Listado de palabra con su definicion
-const palabras = [{palabra: "abajarse", definicion: "Acción de bajarse" ,ejemplo: "Vos chavalo abajate de alli"},
-                    {palabra: "abanico", definicion: "ventilador", ejemplo: "Dice mi mama que apagues el abanico"},
-                    {palabra: "abierta", definicion: " irse de un lugar",ejemplo: "Ese mae se dio la abierta cuando te vio"},
-                    {palabra: "acá", definicion: "lo mismo que aquí",ejemplo: "chavalo veni para acá"},
-                    {palabra: "acalambrado", definicion: "tener miedo",ejemplo: "Ese mae te tiene acalambrado a vos"},
-                    {palabra: " achantado", definicion: "triste",ejemplo: "Te tiene achantado la jaña mae"},
-                    {palabra: "afincar", definicion: "besar (de manera más intensa o de caracter sexual)",ejemplo: "Vos afincaste a la mae"},
-                    {palabra: "invivible",definicion: "persona que no vive ni deja vivir", ejemplo: " mae que invivible que sos- Ana"}];
+const palabras = [{ palabra: "abajarse", definicion: "Acción de bajarse", ejemplo: "Vos chavalo abajate de alli" },
+    { palabra: "abanico", definicion: "ventilador", ejemplo: "Dice mi mama que apagues el abanico" },
+    { palabra: "abierta", definicion: " irse de un lugar", ejemplo: "Ese mae se dio la abierta cuando te vio" },
+    { palabra: "acá", definicion: "lo mismo que aquí", ejemplo: "chavalo veni para acá" },
+    { palabra: "acalambrado", definicion: "tener miedo", ejemplo: "Ese mae te tiene acalambrado a vos" },
+    { palabra: " achantado", definicion: "triste", ejemplo: "Te tiene achantado la jaña mae" },
+    { palabra: "afincar", definicion: "besar (de manera más intensa o de caracter sexual)", ejemplo: "Vos afincaste a la mae" },
+    { palabra: "invivible", definicion: "persona que no vive ni deja vivir", ejemplo: " mae que invivible que sos- Ana" }
+];
 const entrada_busqueda = document.getElementById("busqueda");
 entrada_busqueda.addEventListener("input", (e) => {
-let valor=e.target.value;
-if(valor&& valor.trim().length>0){
-   valor=valor.trim().toLowerCase();
-    const resultados=palabras.filter(palabra => palabra.palabra.toLowerCase().includes(valor)).slice(0,5);
-   //console.log(resultados);
-   renderResults(resultados);
-}
-else
-{
-//console.log("no hay resultados");
-clearResults();
-}
+    let valor = e.target.value;
+    if (valor && valor.trim().length > 0) {
+        valor = valor.trim().toLowerCase();
+        const resultados = palabras.filter(palabra => palabra.palabra.toLowerCase().includes(valor)).slice(0, 5);
+        //console.log(resultados);
+        renderResults(resultados);
+    } else {
+        //console.log("no hay resultados");
+        clearResults();
+    }
 
 });
 /*render a list of results*/
@@ -49,6 +48,3 @@ const renderResults = (results) => {
 const clearResults = () => {
     document.getElementById('resultados').innerHTML = '';
 }
-
-
-   
